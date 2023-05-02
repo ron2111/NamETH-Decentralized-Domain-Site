35d387dc7c9d9354f033741c4550d1c18d2967b4
Rohan Sharma <onlyrohan21@gmail.com>
2023-05-02 05:42:59 +0530

    test setup

--- contracts/NamETH.sol
+++ contracts/NamETH.sol
@@ -1,10 +1,17 @@
  // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract NamETH {
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+// using openzeppelin for the erc721 smart contract (nft0)
+contract NamETH is ERC721{  //Inheritance
+
+address public owner;
 
 // state variable to call the contract
-string public name = "NamETH";
+constructor( string memory _name, string memory _symbol) ERC721(_name,_symbol) // ERC 721 constructor
+{
+owner = msg.sender;
 
+}
 
 }
--- test/NamETH.js
+++ test/NamETH.js
@@ -5,12 +5,38 @@ const tokens = (n) => {
 };
 
 describe("NamETH", () => {
-  it("has a name", async () => {
-    const NamETH = await ethers.getContractFactory("NamETH"); //gest the js version of smart contract with the help of ethers library
-    let namETH = await NamETH.deploy(); // then we deploy it
+  let namETH;
+  let deployer, owner1;
 
-    // now as it is deployed we can use/test its js functions here, so we have got result variable to get the name property of smart contract gthat we declared earlier in the contract
-    const result = await namETH.name();
-    expect(result).to.equal("NamETH"); // here we test that the result should be same as the original name, then we declare the test as passed
+  const NAME = "NamETH";
+  const SYMBOL = "NETH";
+  beforeEach(async () => {
+    // run before each individual test
+
+    // signers are the accounts associated with the hardjat project, hardhat provides 20 test accounts as such with 10000 ETH
+    [deployer, owner1] = await ethers.getSigners();
+
+    const NamETH = await ethers.getContractFactory("NamETH"); //gest the js version of smart contract with the help of ethers library
+    namETH = await NamETH.deploy("NamETH", "NETH"); // then we deploy it
+  });
+
+  describe("Deployement", () => {
+    // deployement tests grouped
+
+    it("has a name", async () => {
+      // now as it is deployed we can use/test its js functions here, so we have got result variable to get the name property of smart contract gthat we declared earlier in the contract
+      const result = await namETH.name();
+      expect(result).to.equal(NAME); // here we test that the result should be same as the original name, then we declare the test as passed
+    });
+
+    it("has a symbol", async () => {
+      const result = await namETH.symbol(); // symbol smoke test
+      expect(result).to.equal(SYMBOL);
+    });
+
+    it("sets the owner", async () => {
+      const result = await namETH.owner(); // owner smoke test
+      expect(result).to.equal(deployer.address);
+    });
   });
 });
