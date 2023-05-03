9b22bbda430d05d086e75d41342c2f13f3296860
Rohan Sharma <onlyrohan21@gmail.com>
2023-05-03 07:15:16 +0530

    contract-deployed

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,11 +7,48 @@
 const hre = require("hardhat");
 
 const tokens = (n) => {
-  return ethers.utils.parseUnits(n.toString(), 'ether')
-}
+  return ethers.utils.parseUnits(n.toString(), "ether");
+};
 
 async function main() {
+  //  Setup accounts & variables
+  const [deployer] = await ethers.getSigners();
+  const NAME = "NamETH";
+  const SYMBOL = "NETH";
 
+  // Deploy Contract
+  const NamETH = await ethers.getContractFactory("NamETH"); //gets the js version of smart contract with the help of ethers library
+  const namETH = await NamETH.deploy(NAME, SYMBOL);
+  await namETH.deployed();
+
+  console.log(`Deployed Domain Contract at: ${namETH.address}\n`);
+
+  // List 6 domains (pre-minted)
+  const names = [
+    "rohan.eth",
+    "sanchit.eth",
+    "mudit.eth",
+    "viren.eth",
+    "pragati.eth",
+    "mamthur.eth",
+  ];
+  const costs = [
+    // ether price
+    tokens(10),
+    tokens(25),
+    tokens(15),
+    tokens(2.5),
+    tokens(3),
+    tokens(1),
+  ];
+
+  // minting done beforeHand
+  for (var i = 0; i < 6; i++) {
+    const transaction = await namETH.connect(deployer).list(names[i], costs[i]);
+    await transaction.wait();
+
+    console.log(`Listed Domain ${i + 1}: ${names[i]}`);
+  }
 }
 
 main().catch((error) => {
